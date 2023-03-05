ab711150ab705464219781f052867dcfa2e9e866
shota <morgen.ist.auch.noch.ein.tag@gmail.com>
2023-03-05 16:39:11 +0900

    fix:

--- popup/index.tsx
+++ popup/index.tsx
@@ -1,3 +1,4 @@
+import image from "data-base64:~assets/img/car_taxi_black.png"
 import { useState } from "react"
 
 function IndexPopup() {
@@ -15,8 +16,9 @@ function IndexPopup() {
         <a href="https://www.plasmo.com" target="_blank">
           Plasmo
         </a>{" "}
-        Extension!
+        Extension!hoge!
       </h2>
+      <img src={image} alt="hogehoge" />
       <input onChange={(e) => setData(e.target.value)} value={data} />
       <a href="https://docs.plasmo.com" target="_blank">
         View Docs
